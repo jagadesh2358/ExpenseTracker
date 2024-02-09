69facc9c45c24efe926102cfb51957275487089d
jagadesh2358 <107097509+jagadesh2358@users.noreply.github.com>
2024-02-09 09:24:13 +0530

    Add files via upload

---
+++ ExpenseTracker/expense/Expense.java
@@ -0,0 +1,52 @@
+package expense;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Expense implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	private Date date;
+    private String category;
+    private double amount;
+   
+    public Expense(Date date, String category, double amount)
+    {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+       
+    }
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Expense [date=" + date + ", category=" + category + ", amount=" + amount + "]";
+	}
+    
+    
+	
+}
---
+++ ExpenseTracker/expense/ExpenseManager.java
@@ -0,0 +1,32 @@
+package expense;
+
+import java.util.*;
+
+public class ExpenseManager
+{
+	private List<Expense> expenses;
+//	private static final String EXPENSE_DATA_FILE = "expense_data.txt";
+	
+
+    public ExpenseManager() {
+        this.expenses = new ArrayList<>();
+    }
+
+    public void addExpense(Expense expense) {
+        expenses.add(expense);
+    }
+
+    public List<Expense> listExpenses() {
+        return expenses;
+    }
+
+    public Map<String, Double> getCategoryWiseSummation() {
+        Map<String, Double> categoryWiseSum = new HashMap<>();
+        for (Expense expense : expenses) {
+            String category = expense.getCategory();
+            double amount = expense.getAmount();
+            categoryWiseSum.put(category, categoryWiseSum.getOrDefault(category, 0.0) + amount);
+        }
+        return categoryWiseSum;
+    }
+}
